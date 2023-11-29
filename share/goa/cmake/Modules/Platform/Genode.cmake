ac412fbd26d8e9a2c2e32459a8ec5527296a7164
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2023-11-29 21:52:37 +0100

    cmake: deduplicate -lgcc option
    Since cmake parses the toolchain file multiple times, we must take care
    with executing commands such as `link_libraries` that append to internal
    state.
    
    genodelabs/goa#52

--- share/goa/cmake/Modules/Platform/Genode.cmake
+++ share/goa/cmake/Modules/Platform/Genode.cmake
@@ -1,5 +1,3 @@
-set(GENODE 1)
-
 set(CMAKE_FIND_LIBRARY_SUFFIXES ".lib.so")
 set(CMAKE_FIND_LIBRARY_PREFIXES "")
 set(CMAKE_SHARED_LIBRARY_SUFFIX ".lib.so")
@@ -8,5 +6,12 @@ set(CMAKE_SHARED_LIBRARY_RPATH_LINK_C_FLAG "-Wl,-rpath-link,")
 # library that contains 'dlopen' and friends
 set(CMAKE_DL_LIBS "-l:libc.lib.so")
 
-# force link libgcc to all targets
-link_libraries(gcc)
+# force link libgcc to all targets, yet
+# since this file is parsed multiple times, we must take care to only
+# execute this command once
+if (NOT GENODE)
+	link_libraries(gcc)
+endif()
+
+set(GENODE 1)
+
